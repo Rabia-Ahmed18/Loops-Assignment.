aaad9ae8e63599982f68591b990c3e4e9cbab685
Rabia-Ahmed18 <rabiaahmed16111999@gmail.com>
2024-06-02 18:24:34 -0700

    first commit

---
+++ Q#2/main.ts
@@ -0,0 +1,54 @@
+/*2. Guessing Game (Using while loop )
+Objective: Create a simple number guessing game where the user tries to guess a randomly 
+generated number between 1 and a specified maximum value using a predefined set of guesses.*/
+
+let maxiValue : number= 10;
+
+let randomNum: number =(Math.floor(Math.random() * maxiValue + 1 ));
+console.log(randomNum);
+
+let correctGuess: boolean = false;
+
+let guessedNum :number[] = [ 2 , 3 , 8 , 6 , 5 ,9 , 1];
+
+let n = 0;
+while (!correctGuess && n < guessedNum.length ) {
+    let currentGuess = guessedNum[n];
+    if( currentGuess === randomNum){
+        correctGuess = true;
+        console.log(`Congrats! You've guessed the correct number.`);
+    }else if(currentGuess < randomNum){
+        console.log(`Your guess ${currentGuess} is too low... Try Again` );
+    }
+    else (currentGuess > randomNum){
+        console.log(`Your guess ${currentGuess} is too high... Try Again` );
+
+    };
+    n++;
+    
+};
+
+console.log(`The random number was ${randomNum}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
